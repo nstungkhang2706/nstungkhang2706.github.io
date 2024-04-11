b5a7ec876a720a670f55737d6d4516c653a9a539
nstungkhang2706 <140127837+nstungkhang2706@users.noreply.github.com>
2024-04-12 01:05:59 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+const navToggleEvent = function (elem) {
+  for (let i = 0; i < elem.length; i++) {
+    elem[i].addEventListener("click", function () {
+      navbar.classList.toggle("active");
+      overlay.classList.toggle("active");
+    });
+  }
+}
+
+navToggleEvent(navElemArr);
+navToggleEvent(navLinks);
+
+
+
+/**
+ * header sticky & go to top
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+
+});
+
+
+function toggleInfo() {
+    var expandedInfo = document.getElementById("expanded-info");
+
+    if (expandedInfo.style.display === "" || expandedInfo.style.display === "none") {
+        expandedInfo.style.display = "block";
+        document.querySelector(".btn-primary").textContent = "Thu Gọn";
+    } else {
+        expandedInfo.style.display = "none";
+        document.querySelector(".btn-primary").textContent = "Hiển Thị Thêm";
+    }
+}
